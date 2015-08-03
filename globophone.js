af69eb75d3965fca46bbd83d319586c3f3eedc41
Jessica Grebenschikov <jessica.greben1@gmail.com>
2015-08-02 19:37:11 -0700

    add placeholder

--- globophone.js
+++ globophone.js
@@ -2,24 +2,32 @@ $( document ).ready(function() {
 	$('.saved').hide();
 	$('.name-error').hide();
 	$('.phone-number-error').hide();
+	isLocalStorageEmpty();
 
-	if (localStorage.length === 0) {
-		$(".delete-btn").hide();
+	function isLocalStorageEmpty() {
+		if (localStorage.length === 0) {
+			$(".delete-btn").hide();
+		} else {
+			var savedName = localStorage.getItem('name');
+			var savedNumber = localStorage.getItem('number');
+			$("#name").attr("value", savedName);
+			$("#number").attr("value", savedNumber);
+		}
 	}
 
 	var displayNameError = function() {
 		$('.name-error').show();
-	}
+	};
 
 	var displayPhoneNumberError = function() {
 		$('.phone-number-error').show();
-	}
+	};
 
 	var isValid = function(phoneNumber) {
 	  var phoneRegEx = /[1-9]{1}\d{9}(?:\d{1})?/;
 	  var digits = phoneNumber.replace(/\D/g, "");
 	  return (digits.match(phoneRegEx) !== null);
-	}
+	};
 
 	var saveInput =function() {
 		if (localStorage) {
@@ -36,31 +44,35 @@ $( document ).ready(function() {
 	      displayPhoneNumberError();
 	    }
 		}
-	}
+	};
 
 	var showSaved = function() {
 		$('.user-info-form').hide();
 		$('.saved').show();
 		var savedName = localStorage.getItem('name');
 		$('.saved-notification').html('Saved: '+ savedName + '\'s Number');
-	}
+	};
 
 	var clearStorage = function() { 
 		localStorage.clear();
-		$('.delete-btn').hide();
-	}
+	};
 
 	$('.save-btn').on('click', function(event) {
 		event.preventDefault();
 		saveInput();
 	});
 
-	$('.back').on('click', function() {
+	$('.back').on('click', function(event) {
+		event.preventDefault();
 		$('.saved').hide();
 		$('.user-info-form').show();
+		isLocalStorageEmpty();
 	});
 
-	$('.delete-btn').on('click', clearStorage());
-
+	$('.delete-btn').on('click', function(event) {
+		event.preventDefault();
+		clearStorage();	
+		isLocalStorageEmpty();
+	});
 
 }); 
